1dae461cb819afdb15e637ffe37be34895dcac80
xxgail <xxxggggail@gmail.com>
2019-09-03 21:07:40 +0800

    test

--- test/test2.go
+++ test/test2.go
@@ -25,15 +25,29 @@ func (ip IPhone) call() {
 	fmt.Println("I am iPhone")
 }
 
+type T struct {
+	S string
+}
+
+func (t *T) call() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.S)
+}
+
 func main() {
 	var phone Phone
 
-	phone = &NokiaPhone{"aa"}
+	phone = NokiaPhone{}
 	describe(phone)
 	phone.call()
 
-	phone = new(IPhone)
+	phone = IPhone{} // new(IPhone) <=> &
+	describe(phone)
 	phone.call()
+
 }
 
 func describe(i Phone) {
